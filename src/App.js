470b5a3db9eefbbceef771740eea6c64fae1a1f3
Abhinav Arya <95561280+itzabhinavarya@users.noreply.github.com>
2024-08-04 23:18:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-    {/*test*/}
+    {/*test...*/}
         <Route path="/" element={<Homepage />} />
         <Route path="/allrepo" element={<AllRepo />} />
       </Routes>
